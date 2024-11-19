e8320dc9c4471d51bff22924b002f53299e2ea89
rhayssakkj <132940117+rhayssakkj@users.noreply.github.com>
2024-11-19 10:04:05 -0300

    Add files via upload

---
+++ flashcard 2b-RHAYSSA/perguntas.js
@@ -0,0 +1,17 @@
+criaCartao (
+    'Música',
+    'Quem é a melhor cantora?', 
+    'Olivia Rodrigo!'
+)
+
+criaCartao (
+    'Olivia Rodrigo',
+    'Qual a melhor música dela?', 
+    'Deja vu'
+)
+
+criaCartao (
+    'Futebol',
+    'Quem é o melhor jogador do mundo (atualmente)?', 
+    'Neymar.'
+)
